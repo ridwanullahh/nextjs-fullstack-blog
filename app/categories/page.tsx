53168c09685d5dd76a5a4121949c59a0eba51319
Robocoders <team@blackbox.ai>
2024-10-07 18:48:47 +0000

    Create category listing page and add link to navigation menu

---
+++ app/categories/page.tsx
@@ -0,0 +1,74 @@
+
+'use client';
+
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { getCategories, Category } from '@/lib/requests';
+import Image from 'next/image';
+import styles from './categoryListingPage.module.css';
+
+const CategoryListingPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState<Category[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const categories = await getCategories();
+        setData(categories);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching categories:', error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <CategoryListSkeleton />
+  ) : (
+    <div className={styles.container}>
+      <h1 className={styles.title}>All Categories</h1>
+      <div className={styles.categories}>
+        {data.map((item, index) => (
+          <Link
+            key={index}
+            href={`/posts?cat=${item.title}`}
+            className={`${styles.category} ${styles[item.slug]}`}
+          >
+            {item.img && (
+              <Image
+                src={item.img}
+                alt=""
+                width={32}
+                height={32}
+                className={styles.image}
+              />
+            )}
+            {item.title}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryListingPage;
+
+export const CategoryListSkeleton = () => {
+  const categoryItems = Array.from({ length: 6 }, (_, index) => (
+    <div key={index} className={styles.skeletonCategory}>
+      <div className={styles.skeletonImage} />
+      <div className={styles.skeletonText}>Category {index + 1}</div>
+    </div>
+  ));
+
+  return (
+    <div className={styles.skeletonContainer}>
+      <h1 className={styles.title}>All Categories</h1>
+      <div className={styles.skeletonCategories}>{categoryItems}</div>
+    </div>
+  );
+};
--- components/menu/Menu.tsx
+++ components/menu/Menu.tsx
@@ -12,6 +12,10 @@ const Menu = () => {
       <h2 className={styles.subtitle}>Discover by topic</h2>
       <h1 className={styles.title}>Categories</h1>
       <MenuCategories />
+      <h2 className={styles.subtitle}>All Categories</h2>
+      <h1 className={styles.title}>
+        <Link href="/categories">View All Categories</Link>
+      </h1>
       <h2 className={styles.subtitle}>Chosen by the editor</h2>
       <h1 className={styles.title}>Editors Pick</h1>
       <MenuPosts withImage={true} />
